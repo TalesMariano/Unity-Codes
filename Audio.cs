9d312a6b221cf7dd128311ed469a4a9ca1a31fda
Tales Mariano <tales-700@hotmail.com>
2017-01-04 01:46:15 -0200

    Update Audio.cs

--- Audio.cs
+++ Audio.cs
@@ -1,4 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class Audio : MonoBehaviour {
+	
+	[Header ("Audio")]
+	public AudioClip[] monsterNoises;
+	protected AudioSource aSource;
+	[Range (0,1)]
+	public float pitchChange = 0.1f;
+	public float criesPerSecond = 0.5f;
+	float soundStep = 0;
+	
   void PlayList(AudioSource aSource, AudioClip[] aList, float pitchChange){
 		
 		aSource.pitch = Random.Range (1 - pitchChange / 2, 1 + pitchChange / 2);
@@ -6,3 +19,5 @@
 		aSource.PlayOneShot ( aList[rand]);
 
 	}
+	
+}
