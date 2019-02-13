09acdae8faae4b3692862db74d247cab79fd3ba6
Tales Mariano <tales-700@hotmail.com>
2019-02-13 19:22:11 -0200

    Create Coorotine.cs

---
+++ Coorotine.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RestartScene : MonoBehaviour
+{
+
+
+	public void HighlightColor (){
+		StopCoroutine("IChangeColor");
+		StartCoroutine(IChangeColor(0.3f, startColor, changeColor));
+
+		StopCoroutine("IChangePos");
+		StartCoroutine(IChangePos(0.3f, startPos, changePos ));
+	}
+
+	IEnumerator IChangeColor(float duration, Color color1, Color color2)
+	{
+
+		float time = 0;
+		while (time / duration < 1)
+		{
+
+			img.color = Color.Lerp(color1, color2, time / duration);
+
+			time += Time.deltaTime;
+			yield return null;
+		}
+	}
+
+	IEnumerator IChangePos(float duration, Vector3 pos1, Vector3 pos2)
+	{
+
+		float time = 0;
+		while (time / duration < 1)
+		{
+
+			transform.localPosition = Vector3.Lerp(pos1, pos2, time / duration);
+
+			time += Time.deltaTime;
+			yield return null;
+		}
+		transform.localPosition = pos2;
+	}
+}
