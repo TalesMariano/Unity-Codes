d21120aff8ec36415f45eb97c9336647c01ba684
Tales Mariano <tales-700@hotmail.com>
2019-04-02 11:24:35 -0300

    Create GetSet.cs

---
+++ GetSet.cs
@@ -0,0 +1,34 @@
+//Origin
+//https://docs.microsoft.com/pt-br/dotnet/csharp/programming-guide/classes-and-structs/using-properties
+
+public class Date
+{
+    private int month = 7;  // Backing store
+
+    public int Month
+    {
+        get
+        {
+            return month;
+        }
+        set
+        {
+            if ((value > 0) && (value < 13))
+            {
+                month = value;
+            }
+        }
+    }
+}
+
+class Employee
+{
+    private string name;
+    public string Name
+    {
+        get
+        {
+            return name != null ? name : "NA";
+        }
+    }
+}
