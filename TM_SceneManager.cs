3b6b351b37bbe36663a727946726ba9848d31057
Tales Mariano <tales-700@hotmail.com>
2019-06-11 14:16:49 -0300

    Update TM_SceneManager.cs

--- TM_SceneManager.cs
+++ TM_SceneManager.cs
@@ -2,7 +2,8 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 //Reference
-//Hololens toolkit
+// Hololens toolkit
+// http://www.alanzucconi.com/2016/03/23/scene-management-unity-5/
 
 
 using UnityEngine;
