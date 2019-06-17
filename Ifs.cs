3b280a4e090b8abcd8f1de0a25b3d97995f67da3
Tales Mariano <tales-700@hotmail.com>
2019-06-17 18:38:40 -0300

    Create Ifs.cs

---
+++ Ifs.cs
@@ -0,0 +1,51 @@
+public class Example
+{
+   public static void Main()
+   {
+   
+      // Switch
+      Color c = (Color) (new Random()).Next(0, 3);
+      switch (c)
+      {
+         case Color.Red:
+            Console.WriteLine("The color is red");
+            break;
+         case Color.Green:
+            Console.WriteLine("The color is green");
+            break;
+         case Color.Blue:
+            Console.WriteLine("The color is blue");   
+            break;
+         default:
+            Console.WriteLine("The color is unknown.");
+            break;   
+      }
+      
+      //Check if all bools[] are true
+      bool CheckBoolList(bool[] bl)
+      {
+          foreach (var item in bl)
+          {
+              if (!item)
+                  return false;
+          }
+          return true;
+      }
+      
+      //Simple Check
+      public void ReceivePassword(string pword)
+      {
+          //Assim não
+         /* if(pword == senha)
+          {
+              buttonPrise.interactable = true;
+          }
+          else
+          {
+              buttonPrise.interactable = false;
+          }*/
+
+          buttonPrise.interactable = pword == senha;
+      }
+   }
+}
