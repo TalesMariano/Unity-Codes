5c8bb2066d89a0f3e1c32af3f35f362e8eec2dfd
Tales Mariano <tales-700@hotmail.com>
2019-08-05 10:31:43 -0300

    Create FilePath.cs

---
+++ FilePath.cs
@@ -0,0 +1,3 @@
+// https://docs.unity3d.com/ScriptReference/Application-streamingAssetsPath.html
+// https://docs.unity3d.com/ScriptReference/Application-persistentDataPath.html
+// https://docs.microsoft.com/pt-br/dotnet/api/system.io.file.exists?view=netframework-4.8
