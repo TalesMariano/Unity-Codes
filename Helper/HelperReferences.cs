318933b533f9259fb087686594b973db2ef4ab05
Tales Mariano <tales-700@hotmail.com>
2020-04-01 15:54:35 -0300

    Update HelperReferences.cs

--- Helper/HelperReferences.cs
+++ Helper/HelperReferences.cs
@@ -2,17 +2,21 @@ using UnityEngine;
 
 public class HelperReferences : MonoBehaviour
 {
+    /// <summary>
+    /// Check if there is any missing references on the monobehavior
+    /// only works on public fields
+    /// </summary>
+    /// <param name="obj"> (this) </param>
     public static void CheckMissingReferences(object obj)
     {
-        var type = obj.GetType();
+        var type = obj.GetType();   // Get Obj type
 
-        foreach (var p in type.GetFields())
+        foreach (var p in type.GetFields()) // For each of its fields
         {
-            if (p.GetValue(obj) == null)
+            if (p.GetValue(obj) == null)    // Check if field is null
             {
                 Debug.LogError("Missing Reference: " + p.Name);
                 return;
             }
         }
     }
-}
